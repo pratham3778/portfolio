6c2d9b30298b5fe7a322cb4ccc49c63370dbd4be
pratham <pratham3778@gmail.com>
2025-01-10 22:18:39 +0530

    updated resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import ums from "../../Assets/Projects/ums.jpeg";
 import wapp from "../../Assets/Projects/wapp.png";
 import vqa from "../../Assets/Projects/vqa.jpeg";
+import st from "../../Assets/Projects/st.jpeg";
 import ProjectCards from "./ProjectCards";
 
 function Projects() {
@@ -21,6 +22,24 @@ function Projects() {
         <Row style={{ justifyContent: "centre", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCards
+              imgPath={st}
+              title="Spending Tracker Application"
+              description="Developed a robust system for tracking personal daily expenses, enabling efficient money management and budgeting. It monitor expenditures on a date-wise basis, add new expenses, and gain insights into their spending habits, improving financial oversight and decision-making."
+              ghLink="https://github.com/pratham3778/Spending-Tracker.git"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCards
+              imgPath={vqa}
+              title="Finance Video Question Answering using Large Language Model"
+              description="Developed chatbot leveraging Large Language Models to provide answers based on user
+queries about video content. Utilized Python libraries and NLP algorithms to ensure keyword
+relevance in the finance domain."
+              ghLink="https://github.com/pratham3778/WeatherApplication"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCards
               imgPath={ums}
               title="University Managment System"
               description="Developed a University Management System with centralized management of student, faculty, courses, and attendance records, enabling efficient addition, viewing, and updating of information."
@@ -33,16 +52,6 @@ function Projects() {
               title="Weather Application"
               description="Built a web-based weather app, which allows users to input a city name and receive real time temperature
 and wind flow information. Utilized an online weather API to fetch and display accurate weather data."
-              ghLink="https://github.com/pratham3778/WeatherApplication"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCards
-              imgPath={vqa}
-              title="Finance Video Question Answering using Large Language Model"
-              description="Developed chatbot leveraging Large Language Models to provide answers based on user
-queries about video content. Utilized Python libraries and NLP algorithms to ensure keyword
-relevance in the finance domain."
               ghLink="https://github.com/pratham3778/WeatherApplication"
             />
           </Col>
